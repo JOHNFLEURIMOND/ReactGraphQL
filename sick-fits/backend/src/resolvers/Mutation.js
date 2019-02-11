6d477c094629887217d15ab12a928c95819fbea0
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2019-02-10 20:21:01 -0500

    New changes to Mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,24 @@
 const Mutation = {
-  createDog(parent, args, ctx, info) {
-    global.dogs = global.dogs || []; 
-    // TODO: create a dog
-    const newDog = { name: args.name };
-    global.dogs.push(newDog);
-    return newDog;
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info,
+    );
+    console.log(item);
   },
 };
 
 module.exports = Mutation;
+
+// createDog(parent, args, ctx, info) {
+//   global.dogs = global.dogs || [];
+//   // TODO: create a dog
+//   const newDog = { name: args.name };
+//   global.dogs.push(newDog);
+//   return newDog;
+// },
